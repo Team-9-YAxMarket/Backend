151402c0befb8ec2c320fa9e685ef3dbdc9b71f9
Ivan Reznichenko <ivreznic@gmail.com>
2023-06-10 09:06:20 +0200

    models are almost ready

--- packing_service/src/db/models.py
+++ packing_service/src/db/models.py
@@ -1,8 +1,10 @@
+import enum
 import uuid
 from datetime import datetime
+from typing import List, Optional
 
 from sqlalchemy import ForeignKey
-from sqlalchemy.dialects.postgresql import BYTEA, TEXT, UUID
+from sqlalchemy.dialects.postgresql import TEXT, UUID
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
 
@@ -17,19 +19,72 @@ class Base(DeclarativeBase):
         return f"{self.__class__.__name__}(id={self.id})"
 
 
-class User(Base):
-    __tablename__ = "users"
+class SKU(Base):
+    __tablename__ = "skus"
 
-    username: Mapped[str] = mapped_column(TEXT)
+    sku: Mapped[TEXT]
+    barcode: Mapped[TEXT]
 
-    def __repr__(self):
-        return (
-            f"{self.__class__.__name__}"
-            f"(id={self.id}, username={self.username})"
-        )
+
+class Carton(Base):
+    __tablename__ = "cartons"
+
+    carton_type: Mapped[TEXT]
+    barcode: Mapped[TEXT]
+
+
+class Prompt(Base):
+    __tablename__ = "prompts"
+
+    prompt: Mapped[TEXT]
+
+
+class Item(Base):
+    class Status(str, enum.Enum):
+        """Статус сессии."""
+
+        ADDED = "added"
+        SCANNED = "scanned"
+        FAULTY = "faulty"
+        ABSENT = "absent"
+
+    __tablename__ = "items"
+
+    status: Mapped[Status]
+    sku_id: Mapped[UUID] = mapped_column(ForeignKey("skus.id"))
+    sku: Mapped["SKU"] = relationship()
+    prompts: Mapped[Optional[List["Prompt"]]]
+    # recommended_carton: Mapped
+
+
+class Order(Base):
+    class Status(str, enum.Enum):
+        """Статус сессии."""
+
+        IS_FORMING = "is_forming"
+        IS_COLLECTING = "is_collecting"
+        IS_READY = "is_ready"
+
+    __tablename__ = "orders"
+
+    status: Mapped[Status]
+    session_id: Mapped[Optional[UUID]] = mapped_column(
+        ForeignKey("sessions.id")
+    )
+    session: Mapped[Optional["Session"]] = relationship(back_populates="order")
+    # items: Mapped
+    # recommended_carton: Mapped
+    # selected_carton: Mapped
 
 
 class Session(Base):
+    class Status(str, enum.Enum):
+        """Статус сессии."""
+
+        OPENED = "started"
+        CLOSED_SUCCESS = "closed_success"
+        CLOSED_PROBLEM = "closed_problem"
+
     __tablename__ = "sessions"
 
     start_at: Mapped[datetime] = mapped_column(
@@ -41,7 +96,7 @@ class Session(Base):
         onupdate=datetime.utcnow(),
         nullable=False,
     )
-    end_at: Mapped[datetime] = mapped_column(
-        nullable=True,
-    )
-    ...
+    end_at: Mapped[datetime] = mapped_column(nullable=True)
+    user_id: Mapped[UUID] = mapped_column(UUID(as_uuid=True), nullable=False)
+    status: Mapped[Status]
+    order: Mapped[Order] = relationship(back_populates="session")
